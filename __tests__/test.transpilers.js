058630af8bae1099cfb93dccfee840a89f0b69f4
Frazonar <snip.coloured@gmail.com>
2022-01-23 14:32:40 +0200

    test: add tests for transpileToPython

--- __tests__/test.transpilers.js
+++ __tests__/test.transpilers.js
@@ -1,4 +1,6 @@
 const uwu = require('../index');
+const { PythonShell } = require('python-shell');
+const should = require('should');
 const { helloUwu, invalidUwu } = require('../lib/constants');
 const expectedOutputString = 'Hewwo Wowwd!';
 
@@ -38,6 +40,37 @@ describe('Transpiler tests', () => {
       it('Returns cell array', () => {
         expect(Array.isArray(hewwoWowwd().cells) &&
           !hewwoWowwd().cells.some((value) => typeof value !== 'number')).toBeTruthy();
+      });
+    });
+  });
+
+  describe('Python transpiler', () => {
+    let outputCode;
+
+    it('Does not throw error when input is a valid program', () => {
+      expect(() => {
+        outputCode = uwu.transpileToPython(helloUwu);
+      }).not.toThrow();
+    });
+
+    it('Generates valid Python code', (done) => {
+      PythonShell.runString(outputCode, null, (err, results) => {
+        if (err) {
+          done(err);
+        }
+        done();
+      });
+    });
+
+    describe('Generated function', () => {
+      it('Returns a correct output string', (done) => {
+        PythonShell.runString(outputCode, null, (err, results) => {
+          if (err) {
+            done(err);
+          }
+          results.should.eql([expectedOutputString]);
+          done();
+        });
       });
     });
   });
