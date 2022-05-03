51f9d8ceba3d964e338e5056dbd18074bdfccc07
Frazonar <snip.coloured@gmail.com>
2022-05-03 15:30:10 +0300

    feat: add fixed memory support to C++ transpiler

--- __tests__/test.transpilers.js
+++ __tests__/test.transpilers.js
@@ -47,45 +47,4 @@ describe('Transpiler tests', () => {
       });
     });
   });
-
-  describe('C++ transpiler', () => {
-    let outputCode;
-
-    it('Does not throw error when input is a valid program', () => {
-      expect(() => {
-        outputCode = uwuTK.transpileToCpp(helloUwu);
-      }).not.toThrow();
-    });
-
-    describe('Generated C++ code', () => {
-      const exeExtension = process.platform === 'win32' ? '.exe' : '';
-      const executableFile = `test${exeExtension}`;
-      const sourceFile = 'test.cpp';
-
-      beforeAll(() => {
-        return fs.writeFile(sourceFile, outputCode);
-      });
-
-      it('Is valid', () => {
-        return expect(cppUtils.compileWithGPlus(sourceFile, executableFile, true)).resolves.toBeDefined();
-      });
-
-      it('Has correct output', (done) => {
-        const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
-        exec(commandToRun)
-          .then(({ stdout, stderr }) => {
-            if (stdout.trim() === expectedOutputString) {
-              done();
-            }
-          })
-          .catch((err) => {
-            done(err);
-          });
-      });
-
-      afterAll(() => {
-        return Promise.all([fs.unlink(sourceFile), fs.unlink(executableFile)]);
-      });
-    });
-  });
 });
