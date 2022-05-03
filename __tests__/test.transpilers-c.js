855d524c478cabf87c4090f71102a37adb408302
Frazonar <snip.coloured@gmail.com>
2022-05-03 15:06:12 +0300

    Improve C transpiler

---
+++ __tests__/test.transpilers-c.js
@@ -0,0 +1,50 @@
+const uwuTK = require('../index');
+const cppUtils = require('cpp-utils');
+const fs = require('fs/promises');
+const process = require('process');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const { helloUwu } = require('../lib/constants');
+const expectedOutputString = 'Hewwo Wowwd!';
+
+describe('C transpiler', () => {
+  let outputCode;
+
+  it('Does not throw error when input is a valid program', () => {
+    expect(() => {
+      outputCode = uwuTK.transpileToC(helloUwu);
+    }).not.toThrow();
+  });
+
+  describe('Generated C code', () => {
+    const exeExtension = process.platform === 'win32' ? '.exe' : '';
+    const executableFile = `test${exeExtension}`;
+    const sourceFile = 'test.c';
+
+    beforeAll(() => {
+      return fs.writeFile(sourceFile, outputCode);
+    });
+
+    it('Is valid', () => {
+      return expect(cppUtils.compileWithGcc(sourceFile, executableFile, true)).resolves.toBeDefined();
+    });
+
+    it('Has correct output', (done) => {
+      const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
+      exec(commandToRun)
+        .then(({ stdout, stderr }) => {
+          if (stdout.trim() === expectedOutputString) {
+            done();
+          }
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    afterAll(() => {
+      return Promise.all([fs.unlink(sourceFile), fs.unlink(executableFile)]);
+    });
+  });
+});
--- __tests__/test.transpilers.js
+++ __tests__/test.transpilers.js
@@ -48,47 +48,6 @@ describe('Transpiler tests', () => {
     });
   });
 
-  describe('C transpiler', () => {
-    let outputCode;
-
-    it('Does not throw error when input is a valid program', () => {
-      expect(() => {
-        outputCode = uwuTK.transpileToC(helloUwu);
-      }).not.toThrow();
-    });
-
-    describe('Generated C code', () => {
-      const exeExtension = process.platform === 'win32' ? '.exe' : '';
-      const executableFile = `test${exeExtension}`;
-      const sourceFile = 'test.c';
-
-      beforeAll(() => {
-        return fs.writeFile(sourceFile, outputCode);
-      });
-
-      it('Is valid', () => {
-        return expect(cppUtils.compileWithGcc(sourceFile, executableFile, true)).resolves.toBeDefined();
-      });
-
-      it('Has correct output', (done) => {
-        const commandToRun = process.platform === 'win32' ? executableFile : `./${executableFile}`;
-        exec(commandToRun)
-          .then(({ stdout, stderr }) => {
-            if (stdout.trim() === expectedOutputString) {
-              done();
-            }
-          })
-          .catch((err) => {
-            done(err);
-          });
-      });
-
-      afterAll(() => {
-        return Promise.all([fs.unlink(sourceFile), fs.unlink(executableFile)]);
-      });
-    });
-  });
-
   describe('C++ transpiler', () => {
     let outputCode;
 
