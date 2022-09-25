14a728ceb0af41e8a26d63b4e8d3cb842fecdc71
synthetic-borealis <snip.coloured@gmail.com>
2022-09-25 15:09:52 +0300

    refactor: refactor javascript compilers

--- src/compilers/JavaScriptNode.ts
+++ src/compilers/JavaScriptNode.ts
@@ -1,107 +1,49 @@
 import genIndent from '../utils/genIndent';
-import tokenizeUwuSource from '../utils/tokenizeUwuSource';
-import LoopBoundaryMismatchError from '../errors/loopBoundaryMismatchError';
-import isValidProgram from '../utils/isValidProgram';
+import compileToJsBase from './JavaScriptBase';
 
-function compileToJsNode(source: string, useDynamicMemory = true, indentSize = 2, indentChar = ' ') {
-  const sourceArray = tokenizeUwuSource(source);
-  if (!isValidProgram(sourceArray)) {
-    throw new LoopBoundaryMismatchError();
-  }
+function compileToJsNode(
+  source: string,
+  isMemoryDynamic = true,
+  mainFunctionName = 'main',
+  indentSize = 2,
+  indentChar = ' ',
+) {
+  const {
+    declaration: declarationLines,
+    definition: definitionLines,
+  } = compileToJsBase(source, isMemoryDynamic, true, indentSize, indentChar);
 
-  let indent = genIndent(1, indentSize, indentChar);
+  const indent = genIndent(1, indentSize, indentChar);
 
-  const getch = [
-    'const fs = require(\'fs\');\n',
-    'function getch() {',
+  const putchar = [
+    'function putchar() {',
     `${indent}const buffer = Buffer.alloc(1);`,
     `${indent}fs.readSync(process.stdin.fd, buffer, 0, 1);`,
     `${indent}return buffer[0];`,
     '}\n',
   ];
-
-  const outputCodeArray = [];
-
-  if (sourceArray.some((val) => val === ',')) {
-    outputCodeArray.push(...getch);
+  const getchar = [
+    'const fs = require(\'fs\');\n',
+    'function getchar() {',
+    `${indent}const buffer = Buffer.alloc(1);`,
+    `${indent}fs.readSync(process.stdin.fd, buffer, 0, 1);`,
+    `${indent}return buffer[0];`,
+    '}\n',
+  ];
+  const hasUserInput = source.indexOf('>w<') > -1;
+  const outputCodeArray: string[] = [];
+  if (hasUserInput) {
+    outputCodeArray.push('const fs = require(\'fs\');\n');
   }
-
-  outputCodeArray.push(
-    'function main() {',
-    `${indent}let position = 0;`,
-  );
-
-  if (useDynamicMemory) {
-    outputCodeArray.push(`${indent}let cells = [0];\n`);
-  } else {
-    outputCodeArray.push(`${indent}let cells = Array(30000).fill(0);\n`);
+  outputCodeArray.push(...declarationLines);
+  outputCodeArray.push(...putchar);
+  if (hasUserInput) {
+    outputCodeArray.push(...getchar);
   }
-
-  let currentDepth = 0;
-
-  sourceArray.forEach((command) => {
-    indent = genIndent(currentDepth + 1, indentSize, indentChar);
-    switch (command) {
-      case 'OwO':
-        if (useDynamicMemory) {
-          outputCodeArray.push(`${indent}if (position + 1 === cells.length) {`);
-          indent += genIndent(1, indentSize, indentChar);
-          outputCodeArray.push(`${indent}cells.push(0);`);
-          indent = genIndent(currentDepth + 1, indentSize, indentChar);
-          outputCodeArray.push('}');
-          outputCodeArray.push(`${indent}++position;\n`);
-        } else {
-          outputCodeArray.push(`${indent}if (position + 1 < cells.length) {`);
-          indent += genIndent(1, indentSize, indentChar);
-          outputCodeArray.push(`${indent}++position;\n`);
-          indent = genIndent(currentDepth + 1, indentSize, indentChar);
-          outputCodeArray.push('}');
-        }
-        break;
-
-      case '°w°':
-        outputCodeArray.push(`${indent}if (position > 0)`);
-        indent += genIndent(1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}--position;\n`);
-        break;
-
-      case 'UwU':
-        outputCodeArray.push(`${indent}if (cells[position] < 255)`);
-        indent += genIndent(1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}++cells[position];\n`);
-        break;
-
-      case 'QwQ':
-        outputCodeArray.push(`${indent}if (cells[position] > 0)`);
-        indent += genIndent(1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}--cells[position];\n`);
-        break;
-
-      case '@w@':
-        outputCodeArray.push(`${indent}process.stdout.write(String.fromCharCode(cells[position]));`);
-        break;
-
-      case '>w<':
-        outputCodeArray.push(`${indent}cells[position] = getch();`);
-        break;
-
-      case '~w~':
-        outputCodeArray.push(`${indent}while (cells[position] > 0) {`);
-        currentDepth += 1;
-        break;
-
-      case '¯w¯':
-        currentDepth = currentDepth > 0 ? currentDepth - 1 : 0;
-        indent = genIndent(currentDepth + 1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}}`);
-        break;
-
-      default:
-    }
-  });
-
-  outputCodeArray.push('}');
-  outputCodeArray.push('main();\n');
+  outputCodeArray.push(`function ${mainFunctionName}() {`);
+  outputCodeArray.push(...(definitionLines.map((line) => `${indent}${line}`)));
+  outputCodeArray.push('}\n');
+  outputCodeArray.push(`${mainFunctionName}();`);
   return outputCodeArray.join('\n');
 }
 
--- src/compilers/JavaScriptWeb.ts
+++ src/compilers/JavaScriptWeb.ts
@@ -1,93 +1,34 @@
 import genIndent from '../utils/genIndent';
-import tokenizeUwuSource from '../utils/tokenizeUwuSource';
-import LoopBoundaryMismatchError from '../errors/loopBoundaryMismatchError';
-import isValidProgram from '../utils/isValidProgram';
+import compileToJsBase from './JavaScriptBase';
 
-function compileToJsWeb(source: string, useDynamicMemory = true, funcName = 'main', indentSize = 2, indentChar = ' ') {
-  const sourceArray = tokenizeUwuSource(source);
-  if (!isValidProgram(sourceArray)) {
-    throw new LoopBoundaryMismatchError();
-  }
+function compileToJsWeb(
+  source: string,
+  isMemoryDynamic = true,
+  mainFunctionName = 'main',
+  indentSize = 2,
+  indentChar = ' ',
+) {
+  const {
+    declaration: declarationLines,
+    definition: definitionLines,
+  } = compileToJsBase(source, isMemoryDynamic, false, indentSize, indentChar);
 
-  let indent = genIndent(1, indentSize, indentChar);
-
+  const indent = genIndent(1, indentSize, indentChar);
   const outputCodeArray = [
-    `function ${funcName}() {`,
-    `${indent}var position = 0;`,
-    `${indent}var output = "";`,
+    ...declarationLines,
+    'let output = \'\';',
   ];
-
-  if (useDynamicMemory) {
-    outputCodeArray.push(`${indent}let cells = [0];\n`);
-  } else {
-    outputCodeArray.push(`${indent}let cells = Array(30000).fill(0);\n`);
-  }
-
-  let currentDepth = 0;
-
-  sourceArray.forEach((command) => {
-    indent = genIndent(currentDepth + 1, indentSize, indentChar);
-    switch (command) {
-      case 'OwO':
-        if (useDynamicMemory) {
-          outputCodeArray.push(`${indent}if (position + 1 === cells.length) {`);
-          indent += genIndent(1, indentSize, indentChar);
-          outputCodeArray.push(`${indent}cells.push(0);`);
-          indent = genIndent(currentDepth + 1, indentSize, indentChar);
-          outputCodeArray.push('}');
-          outputCodeArray.push(`${indent}++position;\n`);
-        } else {
-          outputCodeArray.push(`${indent}if (position + 1 < cells.length) {`);
-          indent += genIndent(1, indentSize, indentChar);
-          outputCodeArray.push(`${indent}++position;\n`);
-          indent = genIndent(currentDepth + 1, indentSize, indentChar);
-          outputCodeArray.push('}');
-        }
-        break;
-
-      case '°w°':
-        outputCodeArray.push(`${indent}if (position > 0)`);
-        indent += genIndent(1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}--position;\n`);
-        break;
-
-      case 'UwU':
-        outputCodeArray.push(`${indent}if (cells[position] < 255)`);
-        indent += genIndent(1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}++cells[position];\n`);
-        break;
-
-      case 'QwQ':
-        outputCodeArray.push(`${indent}if (cells[position] > 0)`);
-        indent += genIndent(1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}--cells[position];\n`);
-        break;
-
-      case '@w@':
-        outputCodeArray.push(`${indent}output += String.fromCharCode(cells[position]);`);
-        break;
-
-      case '~w~':
-        outputCodeArray.push(`${indent}while (cells[position] > 0) {`);
-        currentDepth += 1;
-        break;
-
-      case '¯w¯':
-        currentDepth = currentDepth > 0 ? currentDepth - 1 : 0;
-        indent = genIndent(currentDepth + 1, indentSize, indentChar);
-        outputCodeArray.push(`${indent}}`);
-        break;
-
-      default:
-    }
-  });
-
-  indent = genIndent(1, indentSize, indentChar);
-
+  const putchar = [
+    'function putchar() {',
+    `${indent}output += String.fromCharCode(cells[position]);`,
+    '}\n',
+  ];
+  outputCodeArray.push(...putchar);
+  outputCodeArray.push(`function ${mainFunctionName}() {`);
+  outputCodeArray.push(...(definitionLines.map((line) => `${indent}${line}`)));
   outputCodeArray.push('');
-  outputCodeArray.push(`${indent}return {cells, output};`);
+  outputCodeArray.push(`${indent}return { cells, output };`);
   outputCodeArray.push('}\n');
-
   return outputCodeArray.join('\n');
 }
 
