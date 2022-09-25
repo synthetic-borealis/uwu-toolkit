c33c5ec66802b980df4aa5e72f3857fef723bfa8
synthetic-borealis <snip.coloured@gmail.com>
2022-09-25 15:29:33 +0300

    test: write tests for validation & base javascript compiler

--- src/utils/isValidProgram.ts
+++ src/utils/isValidProgram.ts
@@ -4,8 +4,14 @@
  * @returns {boolean}
  */
 function isValidProgram(sourceArray: string[]): boolean {
-  const numberOfLoopStarts = sourceArray.reduce((previous, current) => ((current === '~w~') ? previous + 1 : previous), 0);
-  const numberOfLoopEnds = sourceArray.reduce((previous, current) => ((current === '¯w¯') ? previous + 1 : previous), 0);
+  const numberOfLoopStarts = sourceArray.reduce(
+    (previous, current) => ((current === '~w~') ? previous + 1 : previous),
+    0,
+  );
+  const numberOfLoopEnds = sourceArray.reduce(
+    (previous, current) => ((current === '¯w¯') ? previous + 1 : previous),
+    0,
+  );
 
   return numberOfLoopStarts === numberOfLoopEnds;
 }
